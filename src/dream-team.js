3998534477c3a53939284c121a3cc4b0817c6dd1
Artem Drozd <artsdrd@gmail.com>
2021-09-09 01:36:58 +0300

    feat: add few solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (Array.isArray(members) !== true) {
+    return false
+  }
+
+  return members.filter(el => typeof(el) === 'string').map(el => el.trim()).map(el => el[0].toUpperCase()).sort().join('')
 }
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,8 +13,24 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    
+    let counter = 1
+    
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        counter += this.calculateDepth(arr.flat())
+        break
+      }
+    }
+
+    /* does not work in case [x, x, [x, []], x, [x, [x, [x]]]]
+     for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        counter += this.calculateDepth(arr[i])
+      }
+    } */
+
+    return counter
   }
 }
