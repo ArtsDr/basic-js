53c30d077289cec89dbec1478e60bcfccc11c6ab
Artem Drozd <artsdrd@gmail.com>
2021-09-14 21:10:26 +0300

    feat: add few solutions

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -22,7 +22,19 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+  let result = {}
+
+  for (let i = 0; i < domains.length; i++) {
+    let reverseDomain = domains[i].split('.').reverse()
+    for (let j = 0, part = ''; j < reverseDomain.length; j++) {
+      part += `.${reverseDomain[j]}`
+      if(result[part]) {
+        result[part]++
+      }else {
+        result[part] = 1
+      }
+    }
+  }
+  return result
 }
