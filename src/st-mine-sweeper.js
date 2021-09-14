a1f3948df2ab994ad2ed20e769d913e79a9d72b6
Artem Drozd <artsdrd@gmail.com>
2021-09-14 18:59:51 +0300

    feat: add st-mine-sweeper solution

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,30 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  let board = []
+
+  for (let i = 0; i < matrix.length; i++) {
+    board[i] = []
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      board[i][j] = 0
+
+      if (i > 0) {
+        if(matrix[i-1][j-1] === true && j > 0) board[i][j]++
+        if(matrix[i-1][j] === true) board[i][j]++
+        if(matrix[i-1][j+1] === true && j < matrix[i].length - 1) board[i][j]++
+      }
+
+      if(matrix[i][j-1] === true && j > 0) board[i][j]++
+      if(matrix[i][j+1] === true && j < matrix[i].length - 1) board[i][j]++
+
+      if (i < matrix.length - 1) {
+        if(matrix[i+1][j-1] === true && j > 0) board[i][j]++
+        if(matrix[i+1][j] === true) board[i][j]++
+        if(matrix[i+1][j+1] === true && j < matrix[i].length - 1) board[i][j]++
+      }
+    }
+  }
+  return board
 }
